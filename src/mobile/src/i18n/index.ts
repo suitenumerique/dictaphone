3cc3ac75364663a63292e018254743d031d37038
Florent Chehab <flo@nextmile.fr>
2026-04-14 19:19:04 +0200

    ✨(mobile) detect user lang

--- src/mobile/src/i18n/index.ts
+++ src/mobile/src/i18n/index.ts
@@ -1,15 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { defaultLanguage, resources } from './resources';
+import { getLocales } from 'react-native-localize';
+
+const possibleLocales = new Set(['fr', 'en']);
+
+const userLocales = getLocales()
+  .map(locale => locale.languageCode.split(' ')[0])
+  .filter(locale => possibleLocales.has(locale));
 
 if (!i18n.isInitialized) {
   i18n.use(initReactI18next).init({
+    lng: userLocales[0] ?? defaultLanguage,
     compatibilityJSON: 'v4',
     fallbackLng: defaultLanguage,
     interpolation: {
       escapeValue: false,
     },
-    lng: defaultLanguage,
     resources,
   });
 }
