6d7c11288f1aa6a6a98f0bfcb88df07a98429a0f
Florent Chehab <flo@nextmile.fr>
2026-06-05 17:53:41 +0200

    🐛(mobile) Android deeplink force setIntent
    On android, sometimes coming back from
    the in app browser login process wouldn't do anything
    because the intent was lost before the react native
    part of the app would start.
    This is an attempt to fix it.

--- src/mobile/android/app/src/main/java/fr/gouv/assistant_transcripts/MainActivity.kt
+++ src/mobile/android/app/src/main/java/fr/gouv/assistant_transcripts/MainActivity.kt
@@ -7,26 +7,42 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import com.zoontek.rnbootsplash.RNBootSplash
+import android.content.Intent
+import android.util.Log
+import android.net.Uri
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "Assistant Transcripts"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "Assistant Transcripts"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
-    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
-    super.onCreate(savedInstanceState)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        val data: Uri? = intent?.data
+//        Log.d("DEEPLINK", "onCreate URI = $data")
+//        Log.d("DEEPLINK", "onCreate Intent = $intent")
+
+        supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+        RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+
+//        val data: Uri? = intent.data
+//        Log.d("DEEPLINK", "onNewIntent URI = $data")
+//        Log.d("DEEPLINK", "onNewIntent Intent = $intent")
+    }
 
 }
