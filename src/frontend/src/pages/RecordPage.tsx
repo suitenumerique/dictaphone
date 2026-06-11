1024be9806364dcafb39c5f38cd0d2445cb546c8
Florent Chehab <flo@nextmile.fr>
2026-06-11 16:02:15 +0200

    ✨(frontend) show alert when recording on web from mobile
    Recording on web from mobile is unstable by nature
    due to the way browsers are handled.
    We want to show a warning to avoid issues.
    
    Closes #95

--- src/frontend/src/pages/RecordPage.tsx
+++ src/frontend/src/pages/RecordPage.tsx
@@ -1,9 +1,21 @@
 import ConnectedLayout from '@/layout/ConnectedLayout.tsx'
 import RecordComponent from '@/features/recordings/components/RecordComponent.tsx'
 import { useTranslation } from 'react-i18next'
+import { useState } from 'react'
+import { Button, Modal, ModalSize } from '@gouvfr-lasuite/cunningham-react'
+import { Download } from '@gouvfr-lasuite/ui-kit'
+import { useLocation } from 'wouter'
+
+const isMobile =
+  // @ts-expect-error Not supported by TypeScript
+  navigator.userAgentData?.mobile ??
+  /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
 
 export default function RecordPage() {
-  const { t } = useTranslation('layout')
+  const { t } = useTranslation(['layout', 'record'])
+  const [showMobileRecordingAlert, setShowMobileRecordingAlert] =
+    useState(isMobile)
+  const [, navigate] = useLocation()
 
   return (
     <ConnectedLayout
@@ -12,7 +24,39 @@ export default function RecordPage() {
       readonly
     >
       <h1 className="sr-only">{t('recordPageH1')}</h1>
-      <RecordComponent />
+      {!showMobileRecordingAlert && <RecordComponent />}
+      <Modal
+        size={ModalSize.MEDIUM}
+        isOpen={showMobileRecordingAlert}
+        onClose={() => setShowMobileRecordingAlert(false)}
+        closeOnClickOutside={true}
+        title={t('record:mobileAlert.title')}
+        rightActions={
+          <>
+            <Button
+              onClick={() => {
+                navigate('/download-mobile-app')
+              }}
+              icon={<Download />}
+              className="btn-centered"
+            >
+              {t('record:mobileAlert.downloadApp')}
+            </Button>
+            <Button
+              onClick={() => {
+                setShowMobileRecordingAlert(false)
+              }}
+              variant="bordered"
+              color="neutral"
+              className="btn-centered"
+            >
+              {t('record:mobileAlert.continueAnyways')}
+            </Button>
+          </>
+        }
+      >
+        <p>{t('record:mobileAlert.description')}</p>
+      </Modal>
     </ConnectedLayout>
   )
 }
